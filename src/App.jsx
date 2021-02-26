6e135a4ca5eb96b15645da686b377ef0878eda44
Ulises <ulises_astrada@outlook.com>
2021-02-25 22:05:43 -0300

    Adding Route with path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/inicio'>
+          Estás en el inicio
+        </Route>
+        <Route path="/">
+          Esta es la URL base
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
