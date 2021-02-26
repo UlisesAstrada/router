78f00c6b686c219be298cefebbaa17606b0222a0
Ulises <ulises_astrada@outlook.com>
2021-02-26 02:17:08 -0300

    Using setTimeout with useEffect

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
-function Base() {
+function Base() { 
   
-  const 
+  const [nombre, setNombre] = useState('Pedro')
   
+  useEffect(() => {
+    setTimeout(() => {
+      setNombre('Ulises')
+    }, 2000)
+  })
+
   return (
     <div>
       <h1>Página de Base Ruta /</h1>
+      {nombre}
     </div>
   )
 }
